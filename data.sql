2a1b7ac973851997d240c56a12148b5def10d1fa
David Underwood <david630@gmail.com>
2023-01-17 16:01:52 -0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data. *
+/* Populate database with sample data. */
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1,'Agumon', 'February 3, 2020', 0, true, 10.23);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2,'Gabumon', 'November 15, 2018', 2, true, 8);
