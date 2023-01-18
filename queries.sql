c4beea0cf7b8df76ef6a88d375741e8f85c0e060
David Underwood <david630@gmail.com>
2023-01-18 12:11:49 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,9 @@ SELECT name, escape_attempts from animals WHERE weight_kg>10.5;
 SELECT * from animals WHERE neutered=true;
 SELECT * from animals WHERE name!='Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unidentified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
