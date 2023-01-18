8ab42450ffa85ed46b0a9e7ff45fb2b11cb8e47c
David Underwood <david630@gmail.com>
2023-01-18 12:52:56 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,3 +27,11 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT check1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT check1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
