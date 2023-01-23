717c017f6aa6e2aa0812e2081b00126824174908
Vanel Nwaba <vanelnwaba@gmail.com>
2023-01-23 20:47:04 +0100

    Add new animal

--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2,'Gabumon', 'November 15, 2018', 2, true, 8);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(3,'Pikachu', 'January 7, 2021', 1, false, 15.04);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(4,'Devimon', 'May 12, 2017', 5, true, 11);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(5,'Papou', 'April 12, 2019', 4, true, 14);
